d17f998dd69f79c85c70453f1884608566f24447
OlgaRomashka <168338544+OlgaRomashka@users.noreply.github.com>
2024-05-01 12:28:04 +0300

    basic folder created

---
+++ 01_Basics/Lesson_01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace Lesson_01
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Task01();
+            SayHello();
+
+        }
+
+
+
+        static void SayHello()
+        {
+            string hello = ("Hello Ivan!");
+            Console.Write(hello);
+        }
+
+        static void Task01()
+        {
+            int i = 5;
+            int j = 7;
+            if (i > j)
+            {
+                Console.Write(i + ", " + j);
+            }
+            else
+            {
+                Console.Write(j + ", " + i);
+            }
+        }
+
+
+    }
+}
