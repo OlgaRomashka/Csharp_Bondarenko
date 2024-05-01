08fc7091804c4250009ef5071e3d469148922acf
OlgaRomashka <168338544+OlgaRomashka@users.noreply.github.com>
2024-05-01 18:40:27 +0300

    tasks 1-6

--- 01_Basics/Lesson_01/Program.cs
+++ 01_Basics/Lesson_01/Program.cs
@@ -7,11 +7,109 @@ namespace Lesson_01
     {
         static void Main()
         {
-            //Task01();
-            SayHello();
+            //SortNumbers();
+            //SayHello();
+            //SayHelloUser();
+            //CalculateAreaSquare();
+            // CalculateAreaCircle();
+            //CalculateTwoNumbers();
 
         }
 
+        static void CalculateTwoNumbers()
+        {
+            double result1;
+            double result2;
+
+            Console.Write("Input first number:");
+            var input1 = Console.ReadLine();
+
+            Console.Write("Input second number:");
+            var input2 = Console.ReadLine();
+
+            bool success1 = double.TryParse(input1, out result1);
+            bool success2 = double.TryParse(input2, out result2);
+
+            if (success1 && success2)
+            {
+                if (result1 > result2)
+                {
+                    Console.Write("result:" + result1 * result2);
+                }
+                else 
+                {
+                    if (result2 != 0)
+                    {
+                        Console.Write("result:" + result1 / result2);
+                    }
+                    else
+                    {
+                        Console.Write("delenie na 0");
+                    }
+                }
+
+            }
+            else
+            {
+                Console.Write("Input is not correct");
+            }
+        }
+
+        static void CalculateAreaCircle()
+        {
+            double PI = Math.PI;
+            Console.Write("Input square radius circle:");
+            var input = Console.ReadLine();
+              double result;
+              bool Success = double.TryParse(input, out result);
+            if (Success)
+            {
+                Console.Write("Square area = " + result * result * PI);
+            }
+            else
+            {
+                Console.Write("Input is not correct");
+            }
+        }
+
+
+        static void CalculateAreaSquare()
+        {
+            Console.Write("Input square side length:");
+
+            var input = Console.ReadLine();
+            double result;
+            bool Success = double.TryParse(input, out result); // true / false
+
+            if (Success)
+            {
+                //если парсинг успешный - считаем площадь
+                Console.Write("Square area = " + result * result);
+            }
+            else
+            {
+                //если парсинг НЕ успешный - ругаем пользователя
+                Console.Write("Input is not correct");
+            }
+
+            //Console.ReadKey();
+        }
+
+
+
+        static void SayHelloUser()
+        {
+            for (var i = 0; i< 14; i++)
+            {
+                Console.Write("=");
+            }
+            Console.WriteLine();
+            Console.WriteLine("/" + "Hello, user!" + "\\");
+            for (var j = 0; j < 16; j++) 
+            {
+                Console.Write("=");
+            }
+        }
 
 
         static void SayHello()
@@ -20,10 +118,11 @@ namespace Lesson_01
             Console.Write(hello);
         }
 
-        static void Task01()
+        static void SortNumbers()
         {
             int i = 5;
             int j = 7;
+
             if (i > j)
             {
                 Console.Write(i + ", " + j);
