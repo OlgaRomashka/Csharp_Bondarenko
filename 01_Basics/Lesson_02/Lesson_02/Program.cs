797980192376d2ae677b261e59110a79c0ee8b9c
OlgaRomashka <168338544+OlgaRomashka@users.noreply.github.com>
2024-05-05 22:20:14 +0300

    hw 2 finished upd

--- 01_Basics/Lesson_02/Lesson_02/Program.cs
+++ 01_Basics/Lesson_02/Lesson_02/Program.cs
@@ -1,271 +1,243 @@
 ﻿using System;
-using System.Data;
-using static System.Net.Mime.MediaTypeNames;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace Lesson_02
 {
     class Program
     {
         static void Main()
         {
-            //TwoNumber();
-            //Calculator();
-            //Fibonacci();
-            //Shop();
+            TwoNumber();
+            Calculator();
+            Fibonacci();
+            Shop();
             MoveSguare();
-            //Console.Clear();
-            //Console.SetCursorPosition(10,5);
-            //Console.Write("# ");
         }
-
-
         static void MoveSguare()
         {
             int posX = 0;
             int posY = 0;
             var isStop = false;
 
-
             while (true)
             {
-
                 Console.Clear();
                 Draw(posX, posY, "# ");
 
                 var key = Console.ReadKey().Key;
 
-                switch (key){
+                switch (key)
+                {
                     case ConsoleKey.LeftArrow:
-                        if (posX > 0) posX--;
-                            break;
-                        
-                    case ConsoleKey.RightArrow:
-                            posX++;
-                            break; 
-                        
-                    case ConsoleKey.UpArrow:
-                        if (posY > 0) posY--;
+                        if (posX > 0) 
+                        { posX--; }
                         break;
-                        
+
+                    case ConsoleKey.RightArrow:
+                        posX++;
+                        break;
+
+                    case ConsoleKey.UpArrow:
+                        if (posY > 0) 
+                        { posY--; }
+                        break;
+
                     case ConsoleKey.DownArrow:
                         posY++;
-                            break;
-                        
+                        break;
+
                     case ConsoleKey.Spacebar:
-                            isStop = true;
-                            break;
-                        
-                    default:
-                            break;
-                        
+                        isStop = true;
+                        break;
+
+                    defaul:
+                        break;
                 }
-
-
                 if (isStop)
                 {
                     break;
                 }
-
             }
-    
         }
         static void Draw(int xPos, int yPos, string s)
         {
             for (var y = 0; y < 10; y++)
             {
-                for(var x = 0; x < 10; x++)
+                for (var x = 0; x < 10; x++)
                 {
-                    Console.SetCursorPosition(xPos + x * s.Length, yPos + y * 3);
+                    Console.SetCursorPosition(xPos + x * s.Length, yPos + y);
                     Console.Write(s);
                 }
 
                 Console.WriteLine();
-
             }
         }
+        //----------------------------------------------------------------------------
+        static void Shop()
+        {
+            double summaProduct = AskForInput("ВВести общую стоимость: ");
 
+            if (summaProduct <= 0)
+            {
+                Console.Write("Товар бесплатный");
+                return;
+            }
+            double differenceProduct = AskForInput("ВВести разницу в стоимости: ");
 
+            if (differenceProduct < 0 || differenceProduct > summaProduct)
+            {
+                Console.Write("Неверные данные");
+                return;
+            }
+            double costPhoduct1 = (summaProduct - differenceProduct) * 0.5;
+            double costPhoduct2 = costPhoduct1 + differenceProduct;
 
+            Console.WriteLine("Стоимость товара МЯЧ: " + costPhoduct1);
+            Console.Write("Стоимость товара БИТА: " + costPhoduct2);
+        }
 
+        static double AskForInput(string text)
+        {
+            string? input;
+            double result;
 
+            do
+            {
+                Console.Write(text);
+                input = Console.ReadLine();
+            }
+            while (!double.TryParse(input, out result));
 
+            return result;
+        }
+        //-------------------------------------------------------------------------
 
+        static void Fibonacci()
+        {
+            string userInput;
+            int length;
 
+            do
+            {
+                Console.Write("Input number fibonacci: ");
+                userInput = Console.ReadLine();
+            } while (!int.TryParse(userInput, out length));
 
-//----------------------------------------------------------------------------
-        //static void Shop()
-        //{
+            int first = 0;
+            int second = 0;
 
-        //    double summaProduct = AskForInput("ВВести общую стоимость: ");
-            
+            for (var i = 0; i < length; i++)
+            {
+                var result = first + second;
 
-        //    if (summaProduct <= 0)
-        //    {
-        //       Console.Write("Товар бесплатный");
-        //        return;
-        //    }
+                Console.Write(result + ", ");
 
-        //    double differenceProduct = AskForInput("ВВести разницу в стоимости: ");
+                if (i == 0)
+                {
+                    second++;
+                }
+                first = second;
+                second = result;
+            }
+        }
+        //-------------------------------------------------------------------------
+        static void Calculator()
+        {
+            double result1;
+            double result2;
 
-        //    if (differenceProduct < 0 || differenceProduct > summaProduct)
-        //           {
-        //             Console.Write("Неверные данные");
-        //             return;
-        //           }
-        //    double costPhoduct1 = (summaProduct - differenceProduct) * 0.5;
-        //    double costPhoduct2 = costPhoduct1 + differenceProduct;
+            Console.Write("Input first number: ");
+            var firstNumber = Console.ReadLine();
 
+            while (!double.TryParse(firstNumber, out result1))
+            {
+                Console.Write("Input first number: ");
+                firstNumber = Console.ReadLine();
+            }
 
-        //    Console.WriteLine("Стоимость товара МЯЧ: " + costPhoduct1);
-        //    Console.Write("Стоимость товара БИТА: " + costPhoduct2);
-        //}
+            Console.Write("Input second number: ");
+            var secondNumber = Console.ReadLine();
 
-        //static double AskForInput(string text)
-        //{
-        //    string? input;
-        //    double result;
+            while (!double.TryParse(secondNumber, out result2))
+            {
+                Console.Write("Input second number: ");
+                secondNumber = Console.ReadLine();
+            }
 
-        //    do
-        //    {
-        //        Console.Write(text);
-        //        input = Console.ReadLine();
-        //    }
-        //    while (!double.TryParse(input, out result));
+            Console.Write("Input calculator symbol:( +, -, *, /, %) ");
+            var symbol = Console.ReadLine();
 
-        //    return result;
-        //}
-//-------------------------------------------------------------------------
+            switch (symbol)
+            {
+                case "+":
+                    Console.Write("Сумма: " + (result1 + result2));
+                    break;
+                case "-":
+                    Console.Write("Сумма: " + (result1 - result2));
+                    break;
+                case "*":
+                    Console.Write("Сумма: " + (result1 * result2));
+                    break;
+                case "/":
+                    if (result2 == 0)
+                    {
+                        Console.WriteLine("Division by zero");
+                        break;
+                    }
+                    Console.Write("Сумма: " + (result1 / result2));
+                    break;
+                case "%":
+                    if (result2 == 0)
+                    {
+                        Console.WriteLine("Remainder division by zero");
+                        break;
+                    }
+                    Console.Write("Сумма: " + (result1 % result2));
+                    break;
+                default:
+                    Console.Write("Invalid calculator symbol");
+                    break;
+            }
+        }
+            //-----------------------------------------------------------------------------
+            static void TwoNumber()
+            {
+                int result1;
+                int result2;
 
-        //static void Fibonacci()
-        //{
-        //    string userInput;
-        //    int length;
+                Console.Write("Input first number: ");
+                var firstNumber = Console.ReadLine();
 
-        //    do
-        //    {
-        //        Console.Write("Input number fibonacci: ");
-        //        userInput = Console.ReadLine();
-        //    } while (!int.TryParse(userInput, out length));
+                while (!int.TryParse(firstNumber, out result1))
+                {
+                    Console.Write("Input first number: ");
+                    firstNumber = Console.ReadLine();
+                }
 
-        //    int first = 0;
-        //    int second = 0;
+                Console.Write("Input second number: ");
+                var secondNumber = Console.ReadLine();
 
-        //    for (var i = 0; i < length; i++)
-        //    {
-        //        var result = first + second;
+                while (!int.TryParse(secondNumber, out result2))
+                {
+                    Console.Write("Input second number: ");
+                    secondNumber = Console.ReadLine();
+                }
 
-        //        Console.Write(result + ", ");
+                switch (result2)
+                {
+                    case 0:
+                        Console.WriteLine("Division by zero");
+                        return;
+                }
 
-        //        if(i == 0)
-        //        {
-        //            second++;
-        //        }
+            var result = result1 % result2;
 
-        //        first = second;
-        //        second = result;
-        //    }
+            var text = (result == 0)
+              ? "Без остатка: " + result1 + " и " + result2
+              : "Остаток: " + result;
 
-        //} 
-//-------------------------------------------------------------------------
-        //static void Calculator()
-        //{
-        //    double result1;
-        //    double result2;
+            Console.Write(text);
 
-        //    Console.Write("Input first number: ");
-        //    var firstNumber = Console.ReadLine();
-
-        //    while (!double.TryParse(firstNumber, out result1))
-        //    {
-        //        Console.Write("Input first number: ");
-        //        firstNumber = Console.ReadLine();
-        //    }
-
-        //    Console.Write("Input second number: ");
-        //    var secondNumber = Console.ReadLine();
-
-        //    while (!double.TryParse(secondNumber, out result2) )
-        //    {
-        //        Console.Write("Input second number: ");
-        //        secondNumber = Console.ReadLine();
-        //    }
-
-
-        //    Console.Write("Input calculator symbol:( +, -, *, /, %) ");
-        //    var symbol = Console.ReadLine();
-
-        //    switch (symbol)
-        //    {
-        //        case "+":
-        //            Console.Write("Сумма: " + (result1 + result2));
-        //            break;
-        //        case "-":
-        //            Console.Write("Сумма: " + (result1 - result2));
-        //            break;
-        //        case "*":
-        //            Console.Write("Сумма: " + (result1 * result2));
-        //            break;
-        //        case "/":
-        //            if (result2 == 0)
-        //            {
-        //                Console.WriteLine("Division by zero");
-        //                break;
-        //            }
-        //            Console.Write("Сумма: " + (result1 / result2));
-        //            break;
-        //        case "%":
-        //            if (result2 == 0)
-        //            {
-        //                Console.WriteLine("Remainder division by zero");
-        //                break;
-        //            }
-        //            Console.Write("Сумма: " + (result1 % result2));
-        //            break;
-        //        default:
-        //            Console.Write("Invalid calculator symbol");
-        //            break;
-
-        //    }
-//-----------------------------------------------------------------------------
-        //static void TwoNumber()
-        //{
-        //    int result1;
-        //    int result2;
-
-        //    Console.Write("Input first number: ");
-        //    var firstNumber = Console.ReadLine();
-
-        //    while (!int.TryParse(firstNumber, out result1))
-        //    {
-        //        Console.Write("Input first number: ");
-        //        firstNumber = Console.ReadLine();
-        //    }
-
-        //    Console.Write("Input second number: ");
-        //    var secondNumber = Console.ReadLine();
-
-        //    while (!int.TryParse(secondNumber, out result2))
-        //    {
-        //        Console.Write("Input second number: ");
-        //        secondNumber = Console.ReadLine();
-        //    }
-
-        //switch (result2)
-        //{
-        //    case 0:
-        //        Console.WriteLine("Division by zero");
-        //        return;
-        //}  
-
-        //    var result = result1 % result2;
-
-        //    var text = (result == 0)
-        //      ? "Без остатка: " + result1 + " и " + result2
-        //      : "Остаток: " + result;
-
-        //    Console.Write(text);
-
-        //}
+        }
     }
+    
 }
 
