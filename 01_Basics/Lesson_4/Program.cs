136afc092546f972d1a5dfdca7529c15ed2242ef
OlgaRomashka <168338544+OlgaRomashka@users.noreply.github.com>
2024-05-19 09:59:21 +0300

    Lesson_04_01_finish

--- 01_Basics/Lesson_4/Program.cs
+++ 01_Basics/Lesson_4/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Drawing;
-using System.Runtime.CompilerServices;
 
 namespace Lesson_04
 {
     internal class Program
     {
         static void Main(string[] args)
+        {
+            CarTesting();
+        }
+       
+
+        private static void CarTesting()
         {
             Car car1 = new Car("Audi", 2001, 1000, Car.Color.Red);
             Car car2 = new Car("Ford", 2002, 2000, Car.Color.Blue);
@@ -24,55 +27,13 @@ namespace Lesson_04
             PrintCar(car1);
             PrintCar(car2);
             PrintCar(car3);
-
         }
-
-       
-
-
         static void PrintCar(Car a)
         {
             string s = a.GetInfoCar();
             Console.WriteLine(s);
         }
+
     }
-
-    public class Car { 
-
-        string _marka;
-        int _year;
-        double _price;
-        Color _color;
-
-        public Car(string marka, int year, double price, Color color)
-        {
-            _marka = marka;
-            _year = year;
-            _price = price;
-            _color = color;
-
-        }
-        public void ChangePrice(double newPrice)
-        {
-            _price = newPrice;
-        }
-        public string GetInfoCar()
-        {
-            var info = "Mapка: " + _marka + ", Год: " + _year + ", Цвет: " + _color + ", Цена: " + _price;
-            return info;
-        }
-        public enum Color
-        {
-            Black,
-            White,
-            Blue,
-            Green,
-            Red
-        }
-
-
-        
-    }
-
 
 }
