317d8e881c433ad47807f268925cdc52624768b5
OlgaRomashka <168338544+OlgaRomashka@users.noreply.github.com>
2024-05-26 13:25:38 +0300

    L_06_finish

---
+++ 01_Basics/L_06/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace L_06
+{
+    public static class Helper
+    {
+        public static int GetRandom(int from, int to)
+        {
+            Random rnd = new Random();
+            return rnd.Next(from, to);
+        }
+    }
+}
---
+++ 01_Basics/L_06/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace L_06
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = Helper.GetRandom(1, 10);
+
+            var mas = new int[x];
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = Helper.GetRandom(0, 100);
+            }
+
+            var l = string.Join(",", mas);
+
+            Console.WriteLine(l);
+
+            //T[] FindAll<T> (T[] array, Predicate<T> match) 
+
+            var evenNumbers = Array.FindAll(mas, x => x % 2 == 0);
+
+            var evens = string.Join(", ", evenNumbers);
+
+            Console.WriteLine(evens);
+        }
+    }
+}
