3f3cba6c909e7469a421721495f78d59dbbe5518
OlgaRomashka <168338544+OlgaRomashka@users.noreply.github.com>
2024-05-29 12:35:30 +0300

    L_07_03_finish

---
+++ 01_Basics/L_07_03/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace L_07_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var foodFactory = new Factory();
+
+            var pizza = foodFactory.GetItem<Pizza>();
+            var sushi = foodFactory.GetItem<Sushi>();
+            var burger = foodFactory.GetItem<Burger>();
+        }
+    }
+
+    public class Factory
+    {
+        public T GetItem<T>()
+            where T : class, new()
+        {
+            return  new T() ;
+        } 
+    }
+
+    public class Pizza
+    {
+    }
+
+    public class Burger
+    {
+    }
+
+    public class Sushi
+    {
+    }
+}
