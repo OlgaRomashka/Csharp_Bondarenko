d82443ec73ea5e5b7f9808a903b3889152aa264b
OlgaRomashka <168338544+OlgaRomashka@users.noreply.github.com>
2024-06-23 15:26:54 +0300

    L_11_finish

---
+++ 01_Basics/L_11/Checkbox.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace L_11
+{
+    public class Checkbox
+    {
+        public event Action Click;
+    
+        public void DoClick()
+        {
+            State = !State;
+
+            Click?.Invoke();
+        }
+
+        public bool State { get; set; }
+    }
+}
---
+++ 01_Basics/L_11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace L_11
+{
+    internal class Program
+    {
+        public delegate void MyDelegate(bool state);
+        static void Main()
+        {
+            var checkbox = new Checkbox();
+
+            checkbox.DoClick();
+            checkbox.DoClick();
+            checkbox.DoClick();
+            checkbox.DoClick();
+            checkbox.DoClick();
+
+            checkbox.Click += () =>
+            {
+                var state = checkbox.State ? "on" : "off";
+
+                Console.WriteLine($"Checkbox {state}");
+            };
+        }
+    }
+}
